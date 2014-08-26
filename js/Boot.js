3731504e458f5203a98ab5f17c05433d632d8289
Ollie Baker <oliverjohnbaker@gmail.com>
2014-08-26 18:30:26 +0100

    Centred preload bar

--- js/Boot.js
+++ js/Boot.js
@@ -29,8 +29,6 @@ ShooterGame.Boot.prototype = {
         this.input.maxPointers = 1;
         this.stage.disableVisibilityChange = true;
 
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
         if (this.game.device.desktop)
         {
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
